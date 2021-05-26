3fc504a482d67a342fdfbf7f7a90ddfaaa496b17
Aigerim Ingco <aikish20@yahoo.com>
2021-05-26 22:40:49 +0600

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,10 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
-
+   //created new project
+        // enabled version control
+        //added gitignore file
+        //made initial commit by keeping track of all file
+        //shared on github
     }
 }
